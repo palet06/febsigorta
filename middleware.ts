b75b59e26ebebdc0b0130fe20dbfb93c01acd149
palet06 <murath31@gmail.com>
2024-03-15 00:54:31 +0300

    14 mart son4

--- middleware.ts
+++ middleware.ts
@@ -6,17 +6,11 @@ export default authMiddleware({
     "/",
     "/insurances",
     "/insurances/(.*)",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
+    
   ],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
-  ],
+  
 });
 
 export const config = {
