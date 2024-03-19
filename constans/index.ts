f7f80b3d42b683cc1e485bd1f201c6dab2728b77
Murat <34002859+palet06@users.noreply.github.com>
2024-03-19 15:25:06 +0300

    Update index.ts

--- constans/index.ts
+++ constans/index.ts
@@ -6,9 +6,10 @@ export const headerLinks = [
     route: "/",
   },
   {
-    label: "Hizmetlerimiz",
-    route: "#insurances",
+    label: "Hakkımızda",
+    route: "/about",
   },
+
   {
     label: "Teklif Al",
     route: "/offer",
@@ -47,9 +48,28 @@ export const collectionData = [
   },
   {
     id: "2",
-    title: "Genel sağlık sigortası",
+    title: "Genel Sağlık Sigortası",
     description:
       "Temel sağlık ihtiyaçlarınızı karşılamak için tasarlanmış bir sigorta türüdür. Doktor ziyaretleri, ilaçlar, hastane masrafları gibi sağlık harcamalarını kapsar.",
+    why: "Toplumsal eşitlik ve adil sağlık hizmetlerinin sağlanmasına katkıda bulunarak herkesin temel sağlık ihtiyaçlarına erişimini destekler. Bu nedenlerden dolayı genel sağlık sigortası, bireylerin sağlık ve finansal güvenliği açısından önemli bir adımdır.",
+    whyq1: "Kapsamlı Sağlık Hizmetleri",
+    whya1:
+      "Genel sağlık sigortası, acil durumlar, rutin kontroller, tedaviler ve hastalıklar için geniş bir kapsama sahiptir. Bu, beklenmedik sağlık sorunlarıyla karşılaşıldığında mali yükü hafifletir.",
+    whyq2: " Ekonomik Güvence",
+    whya2:
+      "Sağlık hizmetlerinin yüksek maliyetleri düşünüldüğünde, genel sağlık sigortası ekonomik güvence sağlar. İleri tedaviler, ameliyatlar veya uzun süreli hastalık dönemlerinde finansal olarak korunmanızı sağlar.",
+    whyq3: "Kapsayıcı ve Eşitlikçi Hizmet",
+    whya3:
+      "Genel sağlık sigortası, sağlık hizmetlerine erişimi artırır ve herkesin temel sağlık ihtiyaçlarına erişimini sağlar. Bu, toplumsal eşitlik ve adil sağlık hizmetlerinin sağlanmasına katkıda bulunur.",
+    sssq1: "Genel sağlık sigortası neyi kapsar?",
+    sssa1:
+      "Genel sağlık sigortası, acil müdahaleler, hastalık ve tedaviler, rutin kontroller, ilaçlar ve bazı rehabilitasyon hizmetlerini kapsar. Ancak, poliçenin detaylarına bağlı olarak kapsam farklılık gösterebilir.",
+    sssq2: "Hangi durumlarda genel sağlık sigortası kullanılabilir?",
+    sssa2:
+      "Genel sağlık sigortası, beklenmedik hastalıklar, kazalar, doğumlar ve genel sağlık bakımı gibi bir dizi durumda kullanılabilir. Ancak, her durum için kapsam ve ödeme detayları farklılık gösterebilir.",
+    sssq3: "Genel sağlık sigortası nasıl alınır ve maliyeti nedir?",
+    sssa3:
+      "Genel sağlık sigortası genellikle devlet tarafından sağlanır ve vatandaşlar ya da sigortalılar tarafından belli bir prim ödenerek alınır. Maliyet, sağlanan kapsama, yaş, gelir ve diğer faktörlere göre değişiklik gösterebilir.",
     image: "/assets/images/sagliksigortasi2.jpg",
     type: "Kişiye Özel",
     link: "/insurances/genel-saglik-sigortasi",
@@ -59,6 +79,25 @@ export const collectionData = [
     title: "Kasko",
     description:
       "Aracınızı olası kazalara, hırsızlığa ve doğal afetlere karşı koruyan bir araç sigortası türüdür. Aracınızın değerini ve sizin güvenliğinizi garanti altına alır.",
+    why: "Kasko sigortası, aracınızı çeşitli risklere karşı korur ve olası zararların maliyetini minimize ederek sürücülerin güvenliğini ve araçlarının değerini korur.",
+    whyq1: "Aracın Güvencesi",
+    whya1:
+      "Kasko sigortası, aracınızı çeşitli tehlikelere karşı korur, bu da hırsızlık, kaza, doğal afetler veya yanma gibi durumlarda aracınızın güvende olmasını sağlar.",
+    whyq2: "Mali Koruma",
+    whya2:
+      " Aracınızın değerine önem veriyorsanız, kasko sigortası mali açıdan koruma sağlar. Kasko sigortası sayesinde, olası zararlar nedeniyle yüksek mali kayıplardan kaçınabilir ve aracınızın tamir veya değiştirilmesi gereken durumlarda mali olarak desteklenirsiniz.",
+    whyq3: "Ekstra Hizmetler",
+    whya3:
+      "Kasko sigortası genellikle yol yardımı, araç kiralama gibi ekstra hizmetler sunar. Bu, aracınızla ilgili beklenmedik durumlarda size ek destek sağlar ve sürücülerin güvenliğini artırır.",
+    sssq1: "Kasko sigortası neyi kapsar?",
+    sssa1:
+      "Kasko sigortası, aracınızın hırsızlık, kaza, doğal afetler, yangın ve benzeri durumlarda oluşabilecek zararları kapsar. Ancak, poliçenin detaylarına göre kapsam farklılık gösterebilir.",
+    sssq2: "Hangi durumlarda kasko sigortası kullanılabilir?",
+    sssa2:
+      "Kasko sigortası, aracınıza yönelik hasarlar için kullanılır. Bunlar, trafik kazaları, hırsızlık, doğal afetlerin neden olduğu zararlar, yangın gibi durumları içerebilir.",
+    sssq3: "Kasko sigortası primleri nasıl belirlenir?",
+    sssa3:
+      "Kasko sigortası primleri aracın değeri, sürücünün profilinin risk düzeyi, kullanım sıklığı, aracın tipi ve kullanılan kapsamlara göre belirlenir. Her durum için primler farklılık gösterebilir.",
     image: "/assets/images/kasko2.jpg",
     type: "Kişiye Özel",
     link: "/insurances/kasko",
@@ -68,6 +107,25 @@ export const collectionData = [
     title: "Zorunlu Trafik Sigortası",
     description:
       "Yasa gereği araç sahiplerinin yaptırmak zorunda olduğu bir sigorta türüdür. Trafik kazalarından kaynaklanan maddi hasarları karşılar.",
+    why: "Zorunlu trafik sigortası, araç sahiplerinin kaza sonucu oluşabilecek maddi ve bedensel zararları karşılamak için yasal olarak yaptırmaları gereken bir güvencedir.",
+    whyq1: "Hukuki Koruma",
+    whya1:
+      "Zorunlu trafik sigortası, araç sahiplerini olası kaza durumlarında hukuki sorumluluklarından korur. Bu sigorta, kaza sonucu oluşan maddi ve bedensel zararları karşılamak için diğer sürücülere ve üçüncü şahıslara karşı mali koruma sağlar.",
+    whyq2: "Toplumsal Güvenlik",
+    whya2:
+      "Zorunlu trafik sigortası, trafikteki diğer kişilerin maddi zararlarını karşılamak için bir güvence sağlar. Bu, trafikteki herkesin kendini ve diğer sürücüleri korumak için önemli bir adımdır ve toplumun genel güvenliğine katkıda bulunur.",
+    whyq3: "Yasal Zorunluluk",
+    whya3:
+      "Zorunlu trafik sigortası, bir araç sahibinin yasal olarak taşıması gereken bir sigortadır. Yasal zorunluluk olarak, trafikte araç kullanırken bu sigortayı yaptırmak ve geçerli tutmak yasalara uyum açısından önemlidir.",
+    sssq1: "Zorunlu trafik sigortası neyi kapsar?",
+    sssa1:
+      "Zorunlu trafik sigortası, kaza sonucu oluşan maddi ve bedensel zararları karşılar. Bu sigorta, diğer sürücülere, yolculara ve üçüncü şahıslara verilen zararları kapsar. Ancak, kendi aracınıza yönelik zararları kapsamaz.",
+    sssq2: "Hangi durumlarda zorunlu trafik sigortası kullanılır?",
+    sssa2:
+      "Zorunlu trafik sigortası, araç sahiplerinin kaza sonucu diğer sürücülere veya üçüncü şahıslara verdiği zararları karşılamak için kullanılır. Kaza sonucu oluşan maddi ve bedensel zararlar bu sigorta kapsamına girer.",
+    sssq3: "Zorunlu trafik sigortası primleri nasıl belirlenir?",
+    sssa3:
+      "Zorunlu trafik sigortası primleri, araç sahibinin risk profiline, aracın özelliklerine ve kullanım durumuna bağlı olarak belirlenir. Sürücünün trafik kazaları geçmişi, aracın yaş ve tipi gibi faktörler primleri etkiler.",
     image: "/assets/images/kasko-trafik.jpg",
     type: "Kişiye Özel",
     link: "/insurances/trafik-sigortasi",
@@ -77,6 +135,25 @@ export const collectionData = [
     title: "Deprem Sigortası",
     description:
       "Ev veya işyerinizin deprem gibi doğal afetlerden kaynaklanan zararlarını karşılayan bir sigorta türüdür. Yapılarınızı güvence altına alır.",
+    why: "Deprem sigortası, deprem sonucu oluşabilecek maddi kayıpları karşılayarak ev veya işyeri sahiplerini beklenmedik felaketlerden kaynaklanabilecek finansal risklere karşı korur.",
+    whyq1: "Maddi Güvence",
+    whya1:
+      " Deprem sigortası, deprem sonucu oluşabilecek yapısal hasarları ve maddi kayıpları karşılar. Bu sigorta, evinizin veya işyerinizin depreme dayanıklı olmayan bir bölgede bulunması durumunda önemli bir mali koruma sağlar.",
+    whyq2: "Risk Yönetimi",
+    whya2:
+      "Deprem sigortası, deprem riskine karşı etkili bir risk yönetim aracıdır. Doğal afetlerin beklenmedik etkileri göz önüne alındığında, deprem sigortasıyla gelecekte oluşabilecek maliyetli hasarlar için hazırlıklı olmak önemlidir.",
+    whyq3: "Toplumsal Dayanışma",
+    whya3:
+      "Deprem sigortası, toplumsal dayanışmayı destekler. Bir felaket durumunda, sigortalılar deprem sonucu oluşan zararlarını karşılamak için sigorta şirketi tarafından ödenen fonlardan faydalanır, böylece toplumun toparlanmasına ve yeniden yapılanmasına katkıda bulunur.",
+    sssq1: "Deprem sigortası neyi kapsar?",
+    sssa1:
+      "Deprem sigortası, deprem sonucu oluşan yapısal hasarları, evin içindeki eşyaların zararlarını ve bazı durumlarda geçici konaklama masraflarını kapsar. Ancak, poliçenin detaylarına bağlı olarak kapsam farklılık gösterebilir.",
+    sssq2: "Hangi durumlarda deprem sigortası kullanılabilir?",
+    sssa2:
+      "Deprem sigortası, deprem sonucu oluşan hasarlar için kullanılır. Bu, yapısal zararlar, eşyaların hasarı ve geçici konaklama masraflarını içerebilir.",
+    sssq3: "Deprem sigortası primleri nasıl belirlenir?",
+    sssa3:
+      "Deprem sigortası primleri, konutun veya işyerinin deprem riski, bölgenin jeolojik yapısı, bina özellikleri ve sigortalının geçmişine bağlı olarak belirlenir. Bu faktörler, primlerin yüksekliği üzerinde etkilidir.",
     image: "/assets/images/deprem.jpg",
     type: "Kişiye Özel",
     link: "/insurances/dask",
@@ -86,6 +163,25 @@ export const collectionData = [
     title: "Seyahat Sigortası",
     description:
       "Yurtiçi veya yurtdışı seyahatlerinizde karşılaşabileceğiniz beklenmedik durumları (kayıp bagaj, acil sağlık hizmetleri, seyahat iptali vb.) kapsayan bir sigorta türüdür.",
+    why: "Seyahat sigortası, seyahat sırasında beklenmedik sağlık sorunları, seyahat iptalleri veya diğer acil durumlarla karşılaşıldığında mali güvence sağlayarak seyahat edenleri korur.",
+    whyq1: "Sağlık Güvencesi",
+    whya1:
+      "Seyahat sigortası, seyahat sırasında beklenmedik sağlık sorunları veya kazalar durumunda tıbbi masrafları karşılar, böylece seyahat edenleri maddi olarak korur.",
+    whyq2: "İptal ve İade Koruması",
+    whya2:
+      "Seyahat sigortası, seyahatin iptali veya kesintiye uğraması durumunda önceden ödenmiş masrafları geri almayı veya yeniden düzenlemeyi sağlar, bu da planların beklenmedik şekilde değişmesi durumunda mali kayıpları azaltır.",
+    whyq3: "Acil Yardım Hizmetleri",
+    whya3:
+      "Seyahat sigortası, seyahat edenlere acil durum durumunda yardım sağlar, kaybolan bagajların bulunması, tıbbi danışmanlık ve 24/7 acil yardım hattı gibi hizmetler sunarak seyahat deneyimini güvenli ve sorunsuz hale getirir.",
+    sssq1: "Seyahat sigortası ne zaman yapılmalıdır?",
+    sssa1:
+      "Seyahat sigortası, seyahat ederken beklenmedik durumlarla karşılaşmanın olası olduğu her durumda yapılmalıdır. Bu, sağlık sorunları, ulaşım iptalleri veya bagaj kaybı gibi beklenmedik olaylar için koruma sağlar.",
+    sssq2: "Seyahat sigortası hangi durumları kapsar?",
+    sssa2:
+      "Seyahat sigortası, sağlık sorunları, seyahatin iptali veya kesintiye uğraması, bagaj kaybı veya hasarı, hırsızlık ve diğer acil durumlar gibi bir dizi durumu kapsar. Ancak, poliçenin detaylarına bağlı olarak kapsam farklılık gösterebilir.",
+    sssq3: "Seyahat sigortası nasıl satın alınır ve ne kadar maliyetlidir?",
+    sssa3:
+      "Seyahat sigortası genellikle seyahat acenteleri, sigorta şirketleri veya çevrimiçi platformlar aracılığıyla satın alınabilir. Maliyet, seyahat süresi, destinasyon, sigortalıların yaşları ve sigorta kapsamı gibi faktörlere bağlı olarak değişir.",
     image: "/assets/images/seyahat.jpg",
     type: "Kişiye Özel",
     link: "/insurances/seyahat-sigortasi",
@@ -95,6 +191,25 @@ export const collectionData = [
     title: "Doğum Sigortası",
     description:
       "Gebelik ve doğum masraflarını karşılayan bir sigorta türüdür. Anne ve bebeğin sağlık hizmetlerine kolay erişim sağlar.",
+    why: "Doğum sigortası, hamilelik ve doğum sürecindeki tıbbi masrafları karşılayarak beklenmedik mali yükleri azaltır ve anne ve bebeğin sağlık hizmetlerine erişimini kolaylaştırır.",
+    whyq1: "Maddi Güvence",
+    whya1:
+      "Doğum sigortası, hamilelik ve doğum sürecinde ortaya çıkabilecek tıbbi masrafları karşılar, böylece beklenmedik mali yükleri azaltır ve aileleri finansal olarak korur.",
+    whyq2: "Sağlık Hizmetlerine Erişim",
+    whya2:
+      "Doğum sigortası, gebelikten doğuma ve sonrasındaki bakıma kadar geniş bir yelpazedeki sağlık hizmetlerine erişimi kolaylaştırır, bu da anne ve bebeğin sağlığını güvence altına alır.",
+    whyq3: "Planlama ve Destek",
+    whya3:
+      "Doğum sigortası, doğum öncesi danışmanlık, doğum koçluğu ve doğum sonrası bakım gibi hizmetleri içerebilir, bu da anne adaylarına ve ailelere gerekli planlama ve destek sağlar.",
+    sssq1: "Doğum sigortası ne zaman yapılmalıdır?",
+    sssa1:
+      "Doğum sigortası, gebe kalmadan önce veya hamilelik başladıktan sonra belirli bir süre içinde yapılmalıdır. En iyi sonuçlar için erken yapılması önerilir.",
+    sssq2: "Doğum sigortası hangi hizmetleri kapsar?",
+    sssa2:
+      "Doğum sigortası, hamilelikten doğuma ve doğum sonrası bakıma kadar bir dizi hizmeti kapsar. Bunlar arasında doğum masrafları, doğum koçluğu, doğum sonrası bakım ve bebek sağlık kontrolleri gibi hizmetler bulunur.",
+    sssq3: "Doğum sigortası primleri nasıl belirlenir?",
+    sssa3:
+      "Doğum sigortası primleri, sigortalının yaşına, sağlık durumuna, sigorta kapsamına ve sigorta şirketine bağlı olarak değişir. Genellikle daha genç ve daha sağlıklı bireyler daha düşük primler öder.",
     image: "/assets/images/doğumsigortasi.jpg",
     type: "Kişiye Özel",
     link: "/insurances/dogum-sigortasi",
@@ -104,6 +219,25 @@ export const collectionData = [
     title: "Hayat Sigortası",
     description:
       "Kişinin vefatı durumunda ailesine veya belirlenen bir kişiye ödeme yapılmasını sağlayan bir sigorta türüdür. Geleceği güvence altına alır.",
+    why: "Hayat sigortası, beklenmedik ölüm durumlarında sevdiklerinize maddi güvence sağlayarak onların finansal geleceklerini korur.",
+    whyq1: "Finansal Güvence",
+    whya1:
+      "Hayat sigortası, sigortalının vefatı durumunda sevdiklerinin maddi ihtiyaçlarını karşılamak için finansal bir güvence sağlar, bu da borçları ödemek, yaşam standartlarını korumak ve geleceklerini güvence altına almak anlamına gelir.",
+    whyq2: "Yatırım ve Birikim",
+    whya2:
+      "Bazı hayat sigortası türleri, prim ödemelerinin bir kısmını yatırım olarak kullanır ve sigortalının yaşamı boyunca birikim yapmasını sağlar. Bu, ileriki dönemlerde acil durumlar için birikim yaparken aynı zamanda hayat sigortası korumasından da faydalanmayı sağlar.",
+    whyq3: "Mirasa Destek",
+    whya3:
+      " Hayat sigortası, miras bırakan kişinin sevdiklerine finansal destek sağlar, böylece vefatı durumunda mirasçılarına güvenli bir gelecek sunar ve onların yaşam standartlarını korur.",
+    sssq1: "Hayat sigortası ne zaman yapılmalıdır?",
+    sssa1:
+      "Hayat sigortası, finansal bağımsızlık, borçların ödenmesi, çocukların eğitimi ve sevdiklerin maddi güvencesi gibi belirli amaçlar için yapılabilir, ancak ne zaman yapılacağı kişisel ve finansal duruma bağlıdır.",
+    sssq2: "Hayat sigortası primleri nasıl belirlenir?",
+    sssa2:
+      "Hayat sigortası primleri, sigortalının yaşına, cinsiyetine, sağlık durumuna, sigorta tutarına ve sigorta süresine bağlı olarak belirlenir. Genellikle genç, sağlıklı bireyler daha düşük primler öder.",
+    sssq3: "Hayat sigortası vefat etmeden önce kullanılabilir mi?",
+    sssa3:
+      "Bazı hayat sigortası türleri, hastalık, sakatlık veya yaşamın son dönemlerinde maddi destek ihtiyacı gibi durumlar için önceden kullanılabilir. Bu tür poliçelerin detayları sigorta şirketlerine ve poliçeye bağlı olarak değişir.",
     image: "/assets/images/hayatsigortasi2.jpg",
     type: "Kişiye Özel",
     link: "/insurances/hayat-sigortasi",
